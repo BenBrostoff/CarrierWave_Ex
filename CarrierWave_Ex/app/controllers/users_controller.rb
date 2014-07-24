75c81efdfc1c995a8fceb5fb382b13c4df3bffc1
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-24 07:24:07 -0400

    Default avatar functional

--- CarrierWave_Ex/app/controllers/users_controller.rb
+++ CarrierWave_Ex/app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < ApplicationController
 
   def index              # size: 800x600
     @users = User.all
+    @user = ""
+  end
+
+  def create
+    @user = User.create(name: params[:name])
+    redirect_to '/'
+  end
+
+
+ def user_params(params)
+    params.permit(:name, :avatar)
   end
 
 end
